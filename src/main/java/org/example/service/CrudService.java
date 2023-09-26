4de116071d1792eec60cc839ec5d8b53b4db65c8
JBraddockm <jbraddockm@gmail.com>
2023-09-26 21:23:29 +0100

    Created CrudService interface and implementing abstract class.

---
+++ src/main/java/org/example/service/CrudService.java
@@ -0,0 +1,16 @@
+package org.example.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CrudService<T, ID> {
+    T save(T t);
+
+    void saveAll(Map<ID, T> map);
+
+    List<T> findAll();
+
+    T findById(ID id);
+
+    void deleteById(ID id);
+}
