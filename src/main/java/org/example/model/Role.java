581a7c32be0fdcf87b24a8bcea8b8309f853afe0
JBraddockm <jbraddockm@gmail.com>
2023-09-26 21:27:05 +0100

    Created Role Entity.

---
+++ src/main/java/org/example/model/Role.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Role {
+    private Long id;
+    private String description;
+}
