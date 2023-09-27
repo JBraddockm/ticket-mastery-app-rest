58d8a671e3531fd374ad5de87b6f60b938dd7042
JBraddockm <jbraddockm@gmail.com>
2023-09-27 23:38:28 +0100

    Added delete operation. Added FlashAttributes and css styles for confirmation.

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,17 +1,19 @@
 package org.example.controller;
 
+import org.example.dto.RoleDTO;
 import org.example.dto.UserDTO;
 import org.example.enums.Gender;
+import org.example.exception.UserNotFoundException;
 import org.example.service.RoleService;
 import org.example.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/user")
@@ -25,18 +27,43 @@ public class UserController {
         this.roleService = roleService;
     }
 
+    @ModelAttribute("genders")
+    public List<Gender> getGenders() {
+        return Arrays.stream(Gender.values()).toList();
+    }
+
+    @ModelAttribute("roles")
+    public List<RoleDTO> getRoles(){
+        return roleService.findAll();
+    }
+
     @GetMapping("/create")
     public String createUser(Model model){
-        model.addAttribute("genders", Arrays.stream(Gender.values()).toList());
-        model.addAttribute("roles", roleService.findAll());
         model.addAttribute("user", new UserDTO());
         model.addAttribute("users", userService.findAll());
         return "user/create";
     }
 
     @PostMapping("/create")
-    public String createUser(@ModelAttribute UserDTO userDTO){
+    public String createUser(@ModelAttribute UserDTO userDTO, RedirectAttributes redirectAttributes){
+
         userService.save(userDTO);
+
+        redirectAttributes.addFlashAttribute("createdUser",userDTO.getUserName());
+
+        return "redirect:/user/create";
+    }
+
+    @GetMapping("{username}/delete")
+    public String deleteUser(@PathVariable("username") String username, RedirectAttributes redirectAttributes){
+        // TODO findByID should return Optional<T>.
+        UserDTO user = Optional.ofNullable(userService.findById(username))
+                .orElseThrow(() -> new UserNotFoundException(username));
+
+        userService.deleteById(username);
+
+        redirectAttributes.addFlashAttribute("deletedUser", user.getUserName());
+
         return "redirect:/user/create";
     }
 }
