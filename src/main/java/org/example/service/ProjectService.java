609a8c1616aae128ecfb75a227f09da2ec01a8f1
JBraddockm <jbraddockm@gmail.com>
2023-10-03 10:57:08 +0100

    Added project complete method.

--- src/main/java/org/example/service/ProjectService.java
+++ src/main/java/org/example/service/ProjectService.java
@@ -3,4 +3,5 @@ package org.example.service;
 import org.example.dto.ProjectDTO;
 
 public interface ProjectService extends CrudService<ProjectDTO, String>{
+    void complete(ProjectDTO projectDTO);
 }
