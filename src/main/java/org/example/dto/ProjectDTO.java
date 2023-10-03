77487aa0e584557ae3815f5e285df4c514ac2f0b
JBraddockm <jbraddockm@gmail.com>
2023-10-03 10:56:04 +0100

    Added dateformat for Thymeleaf and native date input.

--- src/main/java/org/example/dto/ProjectDTO.java
+++ src/main/java/org/example/dto/ProjectDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.enums.Status;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -14,7 +15,9 @@ public class ProjectDTO {
     private String projectName;
     private String projectCode;
     private UserDTO projectManager;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate projectStartDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate projectEndDate;
     private String projectDetail;
     private Status projetStatus;
