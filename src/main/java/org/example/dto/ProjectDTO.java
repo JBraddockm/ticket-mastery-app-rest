3a6d4e19e39ee78897c6f74fe0a112a5cee8e73c
JBraddockm <jbraddockm@gmail.com>
2023-10-14 17:58:14 +0100

    Added field validation.

--- src/main/java/org/example/dto/ProjectDTO.java
+++ src/main/java/org/example/dto/ProjectDTO.java
@@ -1,8 +1,11 @@
 package org.example.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.annotation.ValidDueDate;
 import org.example.enums.Status;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -11,16 +14,41 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@ValidDueDate(
+        startDate = "projectStartDate",
+        endDate = "projectEndDate",
+        message = "Choose a valid due date"
+)
 public class ProjectDTO {
+    @NotBlank(
+            message = "Add project name"
+    )
     private String projectName;
+
+    @NotBlank(
+            message = "Add a project code"
+    )
     private String projectCode;
+
+    @NotNull(
+            message = "Choose a manager"
+    )
     private UserDTO projectManager;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "Choose a start date")
     private LocalDate projectStartDate;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "Choose a due date")
     private LocalDate projectEndDate;
+
+    @NotBlank(
+            message = "Add details about the project"
+    )
     private String projectDetail;
-    private Status projetStatus;
+
+    private Status projectStatus;
 
     private int completeTaskCounts;
     private int unfinishedTaskCounts;
@@ -32,13 +60,13 @@ public class ProjectDTO {
             LocalDate projectStartDate,
             LocalDate projectEndDate,
             String projectDetail,
-            Status projetStatus) {
+            Status projectStatus) {
         this.projectName = projectName;
         this.projectCode = projectCode;
         this.projectManager = projectManager;
         this.projectStartDate = projectStartDate;
         this.projectEndDate = projectEndDate;
         this.projectDetail = projectDetail;
-        this.projetStatus = projetStatus;
+        this.projectStatus = projectStatus;
     }
 }
