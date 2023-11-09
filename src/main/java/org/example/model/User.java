835f9bceb03261c353f9456fd93022f243d75d5b
JBraddockm <jbraddockm@gmail.com>
2023-11-09 00:16:18 +0000

    Created User entity.

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,33 +1,32 @@
 package org.example.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.enums.Gender;
 
-import java.time.LocalDateTime;
-
 @NoArgsConstructor
 @Data
+@Entity
+@Table(
+        name = "users"
+)
 public class User extends BaseEntity{
     private String firstName;
     private String lastName;
     private String userName;
     private String password;
+    private String confirmPassword;
     private String phoneNumber;
+
+    @ManyToOne(
+            fetch = FetchType.LAZY
+    )
     private Role role;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+
     private boolean isEnabled;
 
-
-    public User(Long id, Long insertUserId, Long lastUpdateUserId, LocalDateTime insertDateTime, LocalDateTime lastUpdateDateTime, String firstName, String lastName, String userName, String password, String phoneNumber, Role role, Gender gender, boolean isEnabled) {
-        super(id, insertUserId, lastUpdateUserId, insertDateTime, lastUpdateDateTime);
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-        this.role = role;
-        this.gender = gender;
-        this.isEnabled = isEnabled;
-    }
 }
