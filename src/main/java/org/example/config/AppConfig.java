beeddd045d63a926ed172233aff31fa2a9597e14
JBraddockm <jbraddockm@gmail.com>
2023-11-09 19:08:07 +0000

    Removed unused imports.

--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -1,13 +1,11 @@
 package org.example.config;
 
-import net.datafaker.Faker;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
-
-import java.util.Locale;
 
 @Configuration
 public class AppConfig {
