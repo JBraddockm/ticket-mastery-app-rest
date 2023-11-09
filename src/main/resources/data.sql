48d86ff86634fdbdde5133e2de2b36d4501321b8
JBraddockm <jbraddockm@gmail.com>
2023-11-09 00:09:28 +0000

    Added sample data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO role (description)
+VALUES ('Admin');
+INSERT INTO role (description)
+VALUES ('Employee');
+INSERT INTO role (description)
+VALUES ('User');
+
+insert into users (is_enabled, role_id, first_name, last_name, user_name, gender, password, confirm_password, phone_number) values (false, 3, 'Ferdinand', 'Conaboy', 'fconaboy0@mac.com', 'OTHER', 'oG6<?}Qn', 'oG6<?}Qn', '+447590456900');
