fd1df780c8c5b5095cc80783f914dd261010a874
JBraddockm <jbraddockm@gmail.com>
2023-11-09 00:13:29 +0000

    Implemented GenerationType.SEQUENCE.

--- src/main/java/org/example/model/BaseEntity.java
+++ src/main/java/org/example/model/BaseEntity.java
@@ -1,18 +1,42 @@
 package org.example.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
+@MappedSuperclass
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class BaseEntity {
+    @SequenceGenerator(
+            name = "entity_id_seq",
+            sequenceName = "entity_id_seq",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "entity_id_seq"
+    )
+    @Column(
+            columnDefinition = "SERIAL"
+    )
     private Long id;
+
     private Long insertUserId;
     private Long lastUpdateUserId;
-    private LocalDateTime insertDateTime;
-    private LocalDateTime lastUpdateDateTime;
+
+    @Column(
+            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
+    )
+    private Instant insertDateTime;
+
+    @Column(
+            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
+    )
+    private Instant lastUpdateDateTime;
 }
