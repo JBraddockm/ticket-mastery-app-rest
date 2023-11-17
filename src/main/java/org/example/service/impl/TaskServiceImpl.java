c443252efe01d1519f2d722e474f900e67ed6a6c
JBraddockm <jbraddockm@gmail.com>
2023-11-17 22:18:42 +0000

    Reformatted with google-java-format Used mapping service.

--- src/main/java/org/example/service/impl/TaskServiceImpl.java
+++ src/main/java/org/example/service/impl/TaskServiceImpl.java
@@ -1,78 +1,108 @@
 package org.example.service.impl;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.example.dto.TaskDTO;
 import org.example.dto.UserDTO;
 import org.example.enums.Status;
+import org.example.exception.TaskNotFoundException;
+import org.example.model.Task;
+import org.example.repository.TaskRepository;
 import org.example.service.TaskService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 @Service
-public class TaskServiceImpl extends AbstractMapService<TaskDTO, Long> implements TaskService {
+public class TaskServiceImpl extends AbstractCommonService<Task, TaskDTO> implements TaskService {
 
-    @Override
-    public TaskDTO save(TaskDTO taskDTO) {
-        return super.save(taskDTO.getId(), taskDTO);
-    }
+  private final TaskRepository taskRepository;
 
-    @Override
-    public void saveAll(Map<Long, TaskDTO> map) {
-        super.saveAll(map);
-    }
+  public TaskServiceImpl(ModelMapper modelMapper, TaskRepository taskRepository) {
+    super(modelMapper, Task.class, TaskDTO.class);
+    this.taskRepository = taskRepository;
+  }
 
-    @Override
-    public List<TaskDTO> findAll() {
-        return super.findAll();
-    }
+  @Override
+  public TaskDTO save(TaskDTO taskDTO) {
+    Task task = this.mapToEntity(taskDTO);
+    taskRepository.save(task);
+    return this.mapToDTO(task);
+  }
 
-    @Override
-    public void update(TaskDTO taskDTO) {
-        super.update(taskDTO.getId(), taskDTO);
-    }
+  @Override
+  public List<TaskDTO> findAll() {
+    return taskRepository.findAll().stream().map(this::mapToDTO).toList();
+  }
 
-    @Override
-    public void deleteById(Long id) {
-        super.deleteById(id);
-    }
+  @Override
+  public void update(TaskDTO taskDTO) {
 
-    @Override
-    public TaskDTO findById(Long id) {
-        return super.findById(id);
-    }
+    Task task =
+        taskRepository
+            .findById(taskDTO.getId())
+            .orElseThrow(() -> new TaskNotFoundException(String.valueOf(this)));
 
-    @Override
-    public List<TaskDTO> findTasksByManager(UserDTO manager) {
-        return super.findAll().stream()
-                .filter(taskDTO -> taskDTO.getProject().getProjectManager().equals(manager))
-                .toList();
-    }
+    Task updatedTask = this.mapToEntity(taskDTO);
+    updatedTask.setId(task.getId());
+    updatedTask.setStatus(task.getStatus());
 
-    @Override
-    public List<TaskDTO> findAllTasksByStatus(Status status) {
-        return super.findAll().stream()
-                .filter(taskDTO -> taskDTO.getStatus().equals(status))
-                .toList();
-    }
+    taskRepository.save(updatedTask);
+  }
 
-    @Override
-    public List<TaskDTO> findAllTasksByStatusIsNot(Status status) {
-        return super.findAll().stream()
-                .filter(taskDTO -> !taskDTO.getStatus().equals(status))
-                .toList();
-    }
+  @Override
+  public void updateStatus(TaskDTO taskDTO) {
 
-    @Override
-    public Map<Boolean, List<TaskDTO>> partitionTasksByStatusAndByManager(UserDTO manager) {
-        return this.findTasksByManager(manager).stream()
-                .collect(Collectors.partitioningBy(taskDTO -> taskDTO.getStatus().equals(Status.COMPLETED)));
-    }
+    Task task =
+        taskRepository
+            .findById(taskDTO.getId())
+            .orElseThrow(() -> new TaskNotFoundException(String.valueOf(this)));
 
-    @Override
-    public void updateStatus(TaskDTO task) {
-        super.findById(task.getId()).setStatus(task.getStatus());
-        this.update(task);
-    }
+    task.setStatus(taskDTO.getStatus());
+
+    taskRepository.save(task);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    taskRepository.deleteById(id);
+  }
+
+  @Override
+  public Optional<TaskDTO> findById(Long id) {
+    return taskRepository.findById(id).map(this::mapToDTO);
+  }
+
+  // TODO Rewrite the following methods with JPA queries.
+  @Override
+  public List<TaskDTO> findTasksByManager(UserDTO manager) {
+    return taskRepository.findAll().stream()
+        .map(this::mapToDTO)
+        .filter(taskDTO -> taskDTO.getProject().getProjectManager().equals(manager))
+        .toList();
+  }
+
+  @Override
+  public List<TaskDTO> findAllTasksByStatus(Status status) {
+    return taskRepository.findAll().stream()
+        .map(this::mapToDTO)
+        .filter(taskDTO -> taskDTO.getStatus().equals(status))
+        .toList();
+  }
+
+  @Override
+  public List<TaskDTO> findAllTasksByStatusIsNot(Status status) {
+    return taskRepository.findAll().stream()
+        .map(this::mapToDTO)
+        .filter(taskDTO -> !taskDTO.getStatus().equals(status))
+        .toList();
+  }
+
+  @Override
+  public Map<Boolean, List<TaskDTO>> partitionTasksByStatusAndByManager(UserDTO manager) {
+    return this.findTasksByManager(manager).stream()
+        .collect(
+            Collectors.partitioningBy(taskDTO -> taskDTO.getStatus().equals(Status.COMPLETED)));
+  }
 }
