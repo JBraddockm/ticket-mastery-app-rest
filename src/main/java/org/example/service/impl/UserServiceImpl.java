efd04ad42f4f8d36aa6c5312b16270fdec356ca0
JBraddockm <jbraddockm@gmail.com>
2023-11-17 22:17:34 +0000

    Reformatted with google-java-format Used mapping service

--- src/main/java/org/example/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/service/impl/UserServiceImpl.java
@@ -1,85 +1,82 @@
 package org.example.service.impl;
 
-import org.example.dto.UserDTO;
-import org.example.exception.UserNotFoundException;
-import org.example.mapper.UserMapper;
-import org.example.model.User;
-import org.example.repository.RoleRepository;
-import org.example.repository.UserRepository;
-import org.example.service.UserService;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+import org.example.dto.UserDTO;
+import org.example.exception.UserNotFoundException;
+import org.example.model.User;
+import org.example.repository.UserRepository;
+import org.example.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends AbstractCommonService<User, UserDTO> implements UserService {
 
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
+  private final UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
-        this.userRepository = userRepository;
-        this.userMapper = userMapper;
-    }
+  public UserServiceImpl(ModelMapper modelMapper, UserRepository userRepository) {
+    super(modelMapper, User.class, UserDTO.class);
+    this.userRepository = userRepository;
+  }
 
-    @Override
-    public UserDTO save(UserDTO userDTO) {
-        User user = userMapper.convertToEntity(userDTO);
-        userRepository.save(user);
-        return userMapper.convertToDTO(user);
-    }
+  @Override
+  public UserDTO save(UserDTO userDTO) {
+    User user = this.mapToEntity(userDTO);
+    userRepository.save(user);
+    return this.mapToDTO(user);
+  }
 
-    @Override
-    public List<UserDTO> findAll() {
-        return userRepository.findAll().stream()
-                .map(userMapper::convertToDTO)
-                .toList();
-    }
+  @Override
+  public List<UserDTO> findAll() {
+    return userRepository.findAll().stream().map(this::mapToDTO).toList();
+  }
 
-    @Override
-    public Optional<UserDTO> findByUsername(String userName) {
-        return userRepository.findByUsername(userName)
-                .map(userMapper::convertToDTO);
-    }
+  @Override
+  public Optional<UserDTO> findByUsername(String userName) {
+    return userRepository.findByUsername(userName).map(this::mapToDTO);
+  }
 
-    @Override
-    public void deleteByUsername(String username) {
-        userRepository.deleteByUsername(username);
-    }
+  @Override
+  public void deleteByUsername(String username) {
+    userRepository.deleteByUsername(username);
+  }
 
-    @Override
-    public UserDTO update(UserDTO userDTO) {
+  @Override
+  public UserDTO update(UserDTO userDTO) {
 
-        // Find the user
-        User user = userRepository.findByUsername(userDTO.getUsername()).orElseThrow(() -> new UserNotFoundException(userDTO.getUsername()));
+    // Find the user
+    User user =
+        userRepository
+            .findByUsername(userDTO.getUsername())
+            .orElseThrow(() -> new UserNotFoundException(userDTO.getUsername()));
 
-        // Convert DTO to User
-        User updatedUser = userMapper.convertToEntity(userDTO);
+    // Convert DTO to User
+    User updatedUser = this.mapToEntity(userDTO);
 
-        // Get the User ID and Set it to updated user
-        updatedUser.setId(user.getId());
+    // Get the User ID and Set it to updated user
+    updatedUser.setId(user.getId());
 
-        // Save updated User.
-        userRepository.save(updatedUser);
+    // Save updated User.
+    userRepository.save(updatedUser);
 
-        // Return the updated user
-        return this.findByUsername(updatedUser.getUsername()).orElseThrow();
-    }
+    // Return the updated user
+    return this.findByUsername(updatedUser.getUsername()).orElseThrow();
+  }
 
-    @Override
-    public List<UserDTO> findAllManagers() {
-        return userRepository.findAll().stream()
-                .filter(user -> user.getRole().getId().equals(2L))
-                .map(userMapper::convertToDTO)
-                .toList();
-    }
+  @Override
+  public List<UserDTO> findAllManagers() {
+    return userRepository.findAll().stream()
+        .filter(user -> user.getRole().getId().equals(2L))
+        .map(this::mapToDTO)
+        .toList();
+  }
 
-    @Override
-    public List<UserDTO> findAllEmployees() {
-        return userRepository.findAll().stream()
-                .filter(user -> user.getRole().getId().equals(3L))
-                .map(userMapper::convertToDTO)
-                .toList();
-    }
+  @Override
+  public List<UserDTO> findAllEmployees() {
+    return userRepository.findAll().stream()
+        .filter(user -> user.getRole().getId().equals(3L))
+        .map(this::mapToDTO)
+        .toList();
+  }
 }
