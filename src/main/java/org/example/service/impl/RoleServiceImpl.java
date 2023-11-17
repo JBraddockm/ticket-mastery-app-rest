4dc6b0ebe62e235bb66d9600bc77b51c3eaf3854
JBraddockm <jbraddockm@gmail.com>
2023-11-17 22:15:17 +0000

    Used mapping service.

--- src/main/java/org/example/service/impl/RoleServiceImpl.java
+++ src/main/java/org/example/service/impl/RoleServiceImpl.java
@@ -1,36 +1,34 @@
 package org.example.service.impl;
 
+import java.util.List;
 import org.example.dto.RoleDTO;
 import org.example.exception.RoleNotFoundException;
-import org.example.mapper.RoleMapper;
+import org.example.model.Role;
 import org.example.repository.RoleRepository;
 import org.example.service.RoleService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class RoleServiceImpl implements RoleService {
+public class RoleServiceImpl extends AbstractCommonService<Role, RoleDTO> implements RoleService {
 
-    private final RoleRepository roleRepository;
-    private final RoleMapper roleMapper;
+  private final RoleRepository roleRepository;
 
-    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
-        this.roleRepository = roleRepository;
-        this.roleMapper = roleMapper;
-    }
+  public RoleServiceImpl(ModelMapper modelMapper, RoleRepository roleRepository) {
+    super(modelMapper, Role.class, RoleDTO.class);
+    this.roleRepository = roleRepository;
+  }
 
-    @Override
-    public List<RoleDTO> findAll() {
-        return roleRepository.findAll().stream()
-                .map(roleMapper::convertToDTO)
-                .toList();
-    }
+  @Override
+  public List<RoleDTO> findAll() {
+    return roleRepository.findAll().stream().map(this::mapToDTO).toList();
+  }
 
-    @Override
-    public RoleDTO findById(Long id) {
-        return roleMapper.convertToDTO(roleRepository.findById(id)
-                .orElseThrow(() -> new RoleNotFoundException(String.valueOf(id)))
-        );
-    }
+  @Override
+  public RoleDTO findById(Long id) {
+    return this.mapToDTO(
+        roleRepository
+            .findById(id)
+            .orElseThrow(() -> new RoleNotFoundException(String.valueOf(id))));
+  }
 }
