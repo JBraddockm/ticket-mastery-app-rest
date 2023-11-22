0739101bad67e04e0799ee79c3e592c45a5ca3d3
JBraddockm <jbraddockm@gmail.com>
2023-11-22 23:13:11 +0000

    Implemented custom methods.

--- src/main/java/org/example/service/ProjectService.java
+++ src/main/java/org/example/service/ProjectService.java
@@ -1,20 +1,32 @@
 package org.example.service;
 
-import org.example.dto.ProjectDTO;
-import org.example.dto.UserDTO;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
+import org.example.dto.ProjectDTO;
+import org.example.dto.UserDTO;
+import org.example.enums.Status;
+import org.example.model.Project;
 
-@Transactional
-public interface ProjectService{
-    Optional<ProjectDTO> findByProjectCode(String projectCode);
-    void deleteByProjectCode (String projectCode);
-    ProjectDTO save(ProjectDTO projectDTO);
-    void update(ProjectDTO projectDTO);
-    void complete(ProjectDTO projectDTO);
-    List<ProjectDTO> findAll();
-    List<ProjectDTO> findAllNonCompletedProjects();
-    List<ProjectDTO> getCountedListOfProjectDTO(UserDTO manager);
+public interface ProjectService {
+  Optional<ProjectDTO> findByProjectCode(String projectCode);
+
+  void deleteByProjectCode(String projectCode);
+
+  ProjectDTO save(ProjectDTO projectDTO);
+
+  void update(ProjectDTO projectDTO);
+
+  void complete(ProjectDTO projectDTO);
+
+  List<ProjectDTO> findAll();
+
+  List<ProjectDTO> findAllByProjectManagerIs(UserDTO manager);
+
+  List<ProjectDTO> findAllByProjectStatusIsNot(Status status);
+
+  List<ProjectDTO> getCountedListOfProjectDTO(UserDTO manager);
+
+  ProjectDTO mapToDTO(Project project);
+
+  Project mapToEntity(ProjectDTO projectDTO);
 }
--- src/main/java/org/example/service/impl/ProjectServiceImpl.java
+++ src/main/java/org/example/service/impl/ProjectServiceImpl.java
@@ -1,6 +1,11 @@
 package org.example.service.impl;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.example.dto.ProjectDTO;
+import org.example.dto.TaskDTO;
 import org.example.dto.UserDTO;
 import org.example.enums.Status;
 import org.example.exception.ProjectNotFoundException;
@@ -8,30 +13,35 @@ import org.example.model.Project;
 import org.example.repository.ProjectRepository;
 import org.example.service.ProjectService;
 import org.example.service.TaskService;
+import org.example.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProjectServiceImpl extends AbstractCommonService<Project, ProjectDTO>
     implements ProjectService {
 
-  private final TaskService taskService;
   private final ProjectRepository projectRepository;
+  private final TaskService taskService;
+  private final UserService userService;
 
   public ProjectServiceImpl(
-      TaskService taskService, ProjectRepository projectRepository, ModelMapper modelMapper) {
+      TaskService taskService,
+      ProjectRepository projectRepository,
+      ModelMapper modelMapper,
+      UserService userService) {
     super(modelMapper, Project.class, ProjectDTO.class);
     this.taskService = taskService;
     this.projectRepository = projectRepository;
+    this.userService = userService;
   }
 
   @Override
   public ProjectDTO save(ProjectDTO projectDTO) {
 
-    projectDTO.setProjectStatus(Status.OPEN);
+    if (projectDTO.getProjectStatus() == null) {
+      projectDTO.setProjectStatus(Status.OPEN);
+    }
 
     Project project = this.mapToEntity(projectDTO);
 
@@ -47,15 +57,7 @@ public class ProjectServiceImpl extends AbstractCommonService<Project, ProjectDT
   @Override
   public void update(ProjectDTO projectDTO) {
 
-    Project project =
-        projectRepository
-            .findByProjectCode(projectDTO.getProjectCode())
-            .orElseThrow(() -> new ProjectNotFoundException(projectDTO.getProjectCode()));
-
     Project updatedProject = this.mapToEntity(projectDTO);
-
-    // TODO Check if ID still needed. Add a hidden field to the form if needed.
-    updatedProject.setId(project.getId());
 
     projectRepository.save(updatedProject);
   }
@@ -71,41 +73,46 @@ public class ProjectServiceImpl extends AbstractCommonService<Project, ProjectDT
 
   @Override
   public void complete(ProjectDTO projectDTO) {
-    // TODO
-    projectRepository
-        .findByProjectCode(projectDTO.getProjectCode())
-        .ifPresent(project -> project.setProjectStatus(Status.COMPLETED));
+    Project project =
+        projectRepository
+            .findById(projectDTO.getId())
+            .orElseThrow(() -> new ProjectNotFoundException(projectDTO.getProjectCode()));
+
+    project.setProjectStatus(Status.COMPLETED);
+    projectRepository.save(project);
   }
 
   @Override
-  public List<ProjectDTO> findAllNonCompletedProjects() {
-    return projectRepository.findAll().stream()
-        .filter(projectDTO -> !projectDTO.getProjectStatus().equals(Status.COMPLETED))
+  public List<ProjectDTO> findAllByProjectStatusIsNot(Status status) {
+    return projectRepository.findAllByProjectStatusIsNot(status).stream()
+        .map(this::mapToDTO)
+        .toList();
+  }
+
+  @Override
+  public List<ProjectDTO> findAllByProjectManagerIs(UserDTO manager) {
+    return projectRepository.findAllByProjectManagerIs(userService.mapToEntity(manager)).stream()
         .map(this::mapToDTO)
         .toList();
   }
 
   @Override
   public List<ProjectDTO> getCountedListOfProjectDTO(UserDTO manager) {
-    return projectRepository.findAll().stream()
-        .map(this::mapToDTO)
-        .filter(projectDTO -> projectDTO.getProjectManager().equals(manager))
+    return this.findAllByProjectManagerIs(manager).stream()
         .map(
             projectDTO -> {
+              Map<Boolean, List<TaskDTO>> collect =
+                  projectDTO.getTasks().stream()
+                      .map(taskService::mapToDTO)
+                      .collect(
+                          Collectors.partitioningBy(
+                              taskDTO -> taskDTO.getStatus().equals(Status.COMPLETED)));
 
               // Completed Tasks
-              int completeTaskCounts =
-                  (int)
-                      taskService.partitionTasksByStatusAndByManager(manager).get(true).stream()
-                          .filter(taskDTO -> taskDTO.getProject().equals(projectDTO))
-                          .count();
+              int completeTaskCounts = collect.get(true).size();
 
               // Unfinished Tasks
-              int unfinishedTaskCounts =
-                  (int)
-                      taskService.partitionTasksByStatusAndByManager(manager).get(false).stream()
-                          .filter(taskDTO -> taskDTO.getProject().equals(projectDTO))
-                          .count();
+              int unfinishedTaskCounts = collect.get(false).size();
 
               projectDTO.setCompleteTaskCounts(completeTaskCounts);
               projectDTO.setUnfinishedTaskCounts(unfinishedTaskCounts);
