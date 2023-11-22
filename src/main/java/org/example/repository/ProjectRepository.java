baa348f529b04d50f7461e362d42f3041e394191
JBraddockm <jbraddockm@gmail.com>
2023-11-22 23:12:33 +0000

    Implemented custom derived query methods.

--- src/main/java/org/example/repository/ProjectRepository.java
+++ src/main/java/org/example/repository/ProjectRepository.java
@@ -1,14 +1,21 @@
 package org.example.repository;
 
+import java.util.List;
+import java.util.Optional;
+import org.example.enums.Status;
 import org.example.model.Project;
+import org.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Transactional(readOnly = true)
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     Optional<Project> findByProjectCode(String projectCode);
+
+    List<Project> findAllByProjectStatusIsNot(Status status);
+
+    List<Project> findAllByProjectManagerIs(User manager);
+
     @Transactional
     void deleteByProjectCode(String projectCode);
 }
