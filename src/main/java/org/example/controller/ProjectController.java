a720d83485e197740342afbc58ac86a16e1da07f
JBraddockm <jbraddockm@gmail.com>
2023-11-22 23:11:55 +0000

    Reformatted with google-java-format.

--- src/main/java/org/example/controller/ProjectController.java
+++ src/main/java/org/example/controller/ProjectController.java
@@ -1,10 +1,12 @@
 package org.example.controller;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import org.example.dto.ProjectDTO;
 import org.example.dto.UserDTO;
 import org.example.enums.Status;
 import org.example.exception.ProjectNotFoundException;
+import org.example.exception.UserNotFoundException;
 import org.example.service.ProjectService;
 import org.example.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -13,137 +15,161 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
 
-    private final ProjectService projectService;
-    private final UserService userService;
+  private final ProjectService projectService;
+  private final UserService userService;
 
-    public ProjectController(ProjectService projectService, UserService userService) {
-        this.projectService = projectService;
-        this.userService = userService;
+  public ProjectController(ProjectService projectService, UserService userService) {
+    this.projectService = projectService;
+    this.userService = userService;
+  }
+
+  @ModelAttribute("status")
+  private Status getStatusOpen() {
+    return Status.OPEN;
+  }
+
+  @ModelAttribute("managers")
+  private List<UserDTO> getAllManagers() {
+    return userService.findAllManagers();
+  }
+
+  @GetMapping("all")
+  private String viewAllProjects(Model model) {
+    model.addAttribute("projects", projectService.findAll());
+    return "project/list";
+  }
+
+  @GetMapping("/create")
+  public String newCreateProject(Model model, ProjectDTO project) {
+
+    model.addAttribute("project", project);
+
+    return "project/create";
+  }
+
+  @PostMapping("/create")
+  public String newCreateProject(
+      @Valid @ModelAttribute("project") ProjectDTO project,
+      BindingResult result,
+      Model model,
+      RedirectAttributes redirectAttributes) {
+
+    if (result.hasErrors()) {
+      model.addAttribute(
+          "isValid", "bg-green-50 border-green-500 text-green-900 dark:border-green-400 ");
+      return "project/create";
+    } else {
+      projectService.save(project);
+      redirectAttributes.addFlashAttribute("createdProject", project.getProjectCode());
     }
 
-    @ModelAttribute("status")
-    private Status getStatusOpen() {
-        return Status.OPEN;
+    return "redirect:/project/all";
+  }
+
+  @PostMapping("{projectCode}/delete")
+  public String newDeleteProject(
+      @PathVariable("projectCode") String projectCode, RedirectAttributes redirectAttributes) {
+
+    ProjectDTO deletedProject =
+        projectService
+            .findByProjectCode(projectCode)
+            .orElseThrow(() -> new ProjectNotFoundException(projectCode));
+
+    if (deletedProject.getProjectCode().equals(projectCode)) {
+      projectService.deleteByProjectCode(projectCode);
+      redirectAttributes.addFlashAttribute("deletedProject", deletedProject.getProjectCode());
     }
 
-    @ModelAttribute("managers")
-    private List<UserDTO> getAllManagers() {
-        return userService.findAllManagers();
+    return "redirect:/project/all";
+  }
+
+  @GetMapping("{projectCode}/edit")
+  public String newUpdateProject(@PathVariable("projectCode") String projectCode, Model model) {
+
+    ProjectDTO project =
+        projectService
+            .findByProjectCode(projectCode)
+            .orElseThrow(() -> new ProjectNotFoundException(projectCode));
+
+    model.addAttribute("project", project);
+
+    return "project/update";
+  }
+
+  @PostMapping("{projectCode}/edit")
+  public String newUpdateProject(
+      @Valid @ModelAttribute("project") ProjectDTO project,
+      BindingResult result,
+      @PathVariable("projectCode") String projectCode,
+      RedirectAttributes redirectAttributes,
+      Model model) {
+
+    if (result.hasErrors()) {
+      model.addAttribute(
+          "isValid", "bg-green-50 border-green-500 text-green-900 dark:border-green-400 ");
+      return "project/update";
     }
 
-    @GetMapping("all")
-    private String viewAllProjects(Model model) {
-        model.addAttribute("projects", projectService.findAll());
-        return "project/list";
+    ProjectDTO updatedProject =
+        projectService
+            .findByProjectCode(projectCode)
+            .orElseThrow(() -> new ProjectNotFoundException(projectCode));
+
+    if (project.getProjectCode().equals(projectCode)) {
+      projectService.update(project);
+      redirectAttributes.addFlashAttribute("updatedProject", updatedProject.getProjectCode());
     }
 
-    @GetMapping("/create")
-    public String newCreateProject(Model model, ProjectDTO project) {
+    return "redirect:/project/all";
+  }
 
-        model.addAttribute("project", project);
+  @PostMapping("{projectCode}/complete")
+  public String newCompleteProject(
+      @PathVariable("projectCode") String projectCode, RedirectAttributes redirectAttributes) {
 
-        return "project/create";
+    ProjectDTO completedProject =
+        projectService
+            .findByProjectCode(projectCode)
+            .orElseThrow(() -> new ProjectNotFoundException(projectCode));
+
+    if (completedProject.getProjectCode().equals(projectCode)) {
+      projectService.complete(completedProject);
+      redirectAttributes.addFlashAttribute("completedProject", completedProject.getProjectCode());
     }
 
-    @PostMapping("/create")
-    public String newCreateProject(@Valid @ModelAttribute("project") ProjectDTO project, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+    return "redirect:/project/all";
+  }
 
-        if (result.hasErrors()) {
-            model.addAttribute("isValid", "bg-green-50 border-green-500 text-green-900 dark:border-green-400 ");
-            return "project/create";
-        } else {
-            projectService.save(project);
-            redirectAttributes.addFlashAttribute("createdProject", project.getProjectCode());
-        }
+  @PostMapping("/manager/{projectCode}/complete")
+  public String newManagerCompleteProject(
+      @PathVariable("projectCode") String projectCode, RedirectAttributes redirectAttributes) {
 
-        return "redirect:/project/all";
-    }
+    ProjectDTO completedProject =
+        projectService
+            .findByProjectCode(projectCode)
+            .orElseThrow(() -> new ProjectNotFoundException(projectCode));
 
-    @PostMapping("{projectCode}/delete")
-    public String newDeleteProject(@PathVariable("projectCode") String projectCode, RedirectAttributes redirectAttributes){
+    projectService.complete(completedProject);
+    redirectAttributes.addFlashAttribute("completedProject", completedProject.getProjectCode());
 
-        ProjectDTO deletedProject = projectService.findByProjectCode(projectCode)
-                .orElseThrow(() -> new ProjectNotFoundException(projectCode));
+    return "redirect:/project/manager/project-status";
+  }
 
-        if (deletedProject.getProjectCode().equals(projectCode)) {
-            projectService.deleteByProjectCode(projectCode);
-            redirectAttributes.addFlashAttribute("deletedProject", deletedProject.getProjectCode());
-        }
+  @GetMapping("/manager/project-status")
+  public String newGetProjectByManager(Model model) {
 
-        return "redirect:/project/all";
-    }
+    // TODO Log in will determine the manager after implementing Spring Security.
+    UserDTO manager =
+        userService
+            .findByUsername("johnkelly@example.com")
+            .orElseThrow(() -> new UserNotFoundException("johnkelly@example.com"));
 
-    @GetMapping("{projectCode}/edit")
-    public String newUpdateProject(@PathVariable("projectCode") String projectCode, Model model) {
+    model.addAttribute("projects", projectService.getCountedListOfProjectDTO(manager));
 
-        ProjectDTO project = projectService.findByProjectCode(projectCode)
-                .orElseThrow(() -> new ProjectNotFoundException(projectCode));
-
-        model.addAttribute("project", project);
-
-        return "project/update";
-    }
-
-    @PostMapping("{projectCode}/edit")
-    public String newUpdateProject(@Valid @ModelAttribute("project") ProjectDTO project, BindingResult result, @PathVariable("projectCode") String projectCode, RedirectAttributes redirectAttributes, Model model) {
-
-        if (result.hasErrors()) {
-            model.addAttribute("isValid", "bg-green-50 border-green-500 text-green-900 dark:border-green-400 ");
-            return "project/update";
-        }
-
-        ProjectDTO updatedProject = projectService.findByProjectCode(projectCode)
-                .orElseThrow(() -> new ProjectNotFoundException(projectCode));
-
-        if (updatedProject.getProjectCode().equals(projectCode)) {
-            projectService.update(project);
-            redirectAttributes.addFlashAttribute("updatedProject", project.getProjectCode());
-        }
-
-        return "redirect:/project/all";
-    }
-
-    @PostMapping("{projectCode}/complete")
-    public String newCompleteProject(@PathVariable("projectCode") String projectCode, RedirectAttributes redirectAttributes){
-
-        ProjectDTO completedProject = projectService.findByProjectCode(projectCode)
-                .orElseThrow(() -> new ProjectNotFoundException(projectCode));
-
-        if (completedProject.getProjectCode().equals(projectCode)) {
-            projectService.complete(completedProject);
-            redirectAttributes.addFlashAttribute("completedProject", completedProject.getProjectCode());
-        }
-
-        return "redirect:/project/all";
-
-    }
-
-    @PostMapping("/manager/{projectCode}/complete")
-    public String newManagerCompleteProject(@PathVariable("projectCode") String projectCode, RedirectAttributes redirectAttributes) {
-
-        ProjectDTO completedProject = projectService.findByProjectCode(projectCode)
-                .orElseThrow(() -> new ProjectNotFoundException(projectCode));
-
-        projectService.complete(completedProject);
-        redirectAttributes.addFlashAttribute("completedProject", completedProject.getProjectCode());
-
-        return "redirect:/project/manager/project-status";
-    }
-
-    @GetMapping("/manager/project-status")
-    public String newGetProjectByManager(Model model) {
-
-        // TODO Log in will determine the manager after implementing Spring Security.
-        UserDTO manager = userService.findByUsername("johnkelly@example.com").orElseThrow();
-
-        model.addAttribute("projects", projectService.getCountedListOfProjectDTO(manager));
-
-        return "manager/project-status";
-    }
+    return "manager/project-status";
+  }
 }
