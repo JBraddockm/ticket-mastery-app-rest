898c16ea80efea9d3fe528de9b4ed16b73fda599
JBraddockm <jbraddockm@gmail.com>
2023-11-22 23:09:37 +0000

    Implemented custom derived query methods.

--- src/main/java/org/example/repository/TaskRepository.java
+++ src/main/java/org/example/repository/TaskRepository.java
@@ -1,6 +1,16 @@
 package org.example.repository;
 
+import java.util.List;
+import org.example.enums.Status;
 import org.example.model.Task;
+import org.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {}
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+  List<Task> findTasksByStatusIs(Status status);
+
+  List<Task> findTasksByStatusIsNot(Status status);
+
+  List<Task> findAllByAssignedEmployee(User assignedEmployee);
+}
