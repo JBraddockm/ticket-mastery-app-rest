762359d5b7fd18e2a0ba3bf8d434ce9cacff24f5
JBraddockm <jbraddockm@gmail.com>
2023-11-27 20:29:13 +0000

    Formatted with google-java-format. Added isDeleted field.

--- src/main/java/org/example/model/BaseEntity.java
+++ src/main/java/org/example/model/BaseEntity.java
@@ -12,62 +12,51 @@ import java.time.Instant;
 @NoArgsConstructor
 @Data
 public abstract class BaseEntity {
-    //    @SequenceGenerator(
-//            name = "entity_id_seq",
-//            sequenceName = "entity_id_seq",
-//            allocationSize = 1
-//    )
-//    @Id
-//    @GeneratedValue(
-//            strategy = GenerationType.SEQUENCE,
-//            generator = "entity_id_seq"
-//    )
-//    @Column(
-//            columnDefinition = "SERIAL"
-//    )
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(
-            nullable = false,
-            updatable = false)
-    private Long id;
+  //    @SequenceGenerator(
+  //            name = "entity_id_seq",
+  //            sequenceName = "entity_id_seq",
+  //            allocationSize = 1
+  //    )
+  //    @Id
+  //    @GeneratedValue(
+  //            strategy = GenerationType.SEQUENCE,
+  //            generator = "entity_id_seq"
+  //    )
+  //    @Column(
+  //            columnDefinition = "SERIAL"
+  //    )
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(nullable = false, updatable = false)
+  private Long id;
 
-    @Column(
-            name = "created_on",
-            updatable = false,
-            nullable = false,
-            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
-    )
-    private Instant createdOn;
+  @Column(
+      name = "created_on",
+      updatable = false,
+      nullable = false,
+      columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+  private Instant createdOn;
 
-    @Column(
-            name = "created_by",
-            updatable = false,
-            nullable = false
-    )
-    private Long createdBy;
+  @Column(name = "created_by", updatable = false, nullable = false)
+  private Long createdBy;
 
-    @Column(
-            name = "updated_by"
-    )
+  @Column(name = "updated_by")
+  private Long updatedBy;
 
-    private Long updatedBy;
+  @Column(name = "updated_on", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+  private Instant updatedOn;
 
-    @Column(
-            name = "updated_on",
-            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
-    )
-    private Instant updatedOn;
+  private boolean isDeleted = false;
 
-    @PrePersist
-    public void prePersist() {
-        this.createdBy = 1L;
-        this.createdOn = Instant.now();
-    }
+  @PrePersist
+  public void prePersist() {
+    this.createdBy = 1L;
+    this.createdOn = Instant.now();
+  }
 
-    @PreUpdate
-    private void preUpdate() {
-        this.updatedOn = Instant.now();
-        this.updatedBy = 1L;
-    }
+  @PreUpdate
+  private void preUpdate() {
+    this.updatedOn = Instant.now();
+    this.updatedBy = 1L;
+  }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,35 +1,35 @@
-INSERT INTO role (created_on, created_by, updated_on, updated_by, description)
-VALUES (current_timestamp, 1, null, null, 'Admin'),
-       (current_timestamp, 1, null, null, 'Manager'),
-       (current_timestamp, 1, null, null, 'Employee');
+INSERT INTO role (created_on, created_by, updated_on, updated_by, description, is_deleted)
+VALUES (current_timestamp, 1, null, null, 'Admin', false),
+       (current_timestamp, 1, null, null, 'Manager', false),
+       (current_timestamp, 1, null, null, 'Employee', false);
 
 INSERT INTO users (created_on, created_by, updated_on, updated_by, is_enabled, role_id, first_name, last_name, username,
-                   gender, password, confirm_password, phone_number)
+                   gender, password, confirm_password, phone_number, is_deleted)
 VALUES (current_timestamp, 1, null, null, true, 1, 'Ferdinand', 'Conaboy', 'fconaboy0@mac.com', 'OTHER', 'oG6<?}Qn',
-        'oG6<?}Qn', '+447514234568');
+        'oG6<?}Qn', '+447514234568', false);
 INSERT INTO users (created_on, created_by, updated_on, updated_by, is_enabled, role_id, first_name, last_name, username,
-                   gender, password, confirm_password, phone_number)
+                   gender, password, confirm_password, phone_number, is_deleted)
 VALUES (current_timestamp, 1, null, null, true, 2, 'John', 'Kelly', 'johnkelly@example.com', 'OTHER', 'oG6<?}Qn',
-        'oG6<?}Qn', '+447514234568');
+        'oG6<?}Qn', '+447514234568', false);
 INSERT INTO users (created_on, created_by, updated_on, updated_by, is_enabled, role_id, first_name, last_name, username,
-                   gender, password, confirm_password, phone_number)
+                   gender, password, confirm_password, phone_number, is_deleted)
 VALUES (current_timestamp, 1, null, null, true, 3, 'James', 'Brook', 'jamesbrook@example.com', 'MALE', 'oG6<?}Qn',
-        'oG6<?}Qn', '+447514234568');
+        'oG6<?}Qn', '+447514234568', false);
 
 
 
 INSERT INTO projects (project_end_date, project_start_date, created_by, created_on, manager_id, updated_by,
-                      updated_on, project_code, project_detail, project_name, project_status)
+                      updated_on, project_code, project_detail, project_name, project_status, is_deleted)
 VALUES ('2023-11-30', '2023-11-10', 1, current_timestamp, 2, null, null, 'PR001', 'Introduction to Thymeleaf',
-        'Spring MVC - Thymeleaf', 'OPEN');
+        'Spring MVC - Thymeleaf', 'OPEN', false);
 
 INSERT INTO tasks (assigned_date, assigned_employee_id, created_by, created_on, project_id, updated_by, updated_on,
-                   status, task_detail, task_subject)
-VALUES ('2023-12-30', 3, 1, current_timestamp, 1, null, null, 'OPEN', 'Task Detail', 'Task Sample ONE');
+                   status, task_detail, task_subject, is_deleted)
+VALUES ('2023-12-30', 3, 1, current_timestamp, 1, null, null, 'OPEN', 'Task Detail', 'Task Sample ONE', false);
 
 INSERT INTO tasks (assigned_date, assigned_employee_id, created_by, created_on, project_id, updated_by, updated_on,
-                   status, task_detail, task_subject)
-VALUES ('2023-12-30', 3, 1, current_timestamp, 1, null, null, 'COMPLETED', 'Task Detail', 'Task Sample TWO');
+                   status, task_detail, task_subject, is_deleted)
+VALUES ('2023-12-30', 3, 1, current_timestamp, 1, null, null, 'COMPLETED', 'Task Detail', 'Task Sample TWO', false);
 
 
 
