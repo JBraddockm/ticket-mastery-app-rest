5c1f3e534b01f7a3245d8c374ddece75017e9d90
JBraddockm <jbraddockm@gmail.com>
2023-11-29 18:47:59 +0000

    Added PasswordEncoder as a bean.

--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -3,6 +3,8 @@ package org.example.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
@@ -22,5 +24,10 @@ public class AppConfig {
     resolver.setTemplateMode("XML");
 
     return resolver;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
