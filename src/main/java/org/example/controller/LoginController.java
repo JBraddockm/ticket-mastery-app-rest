1b09ce35a799abc493de4d793b2062158a9bf106
JBraddockm <jbraddockm@gmail.com>
2023-11-29 18:51:35 +0000

    Updated for root controller.

--- src/main/java/org/example/controller/LoginController.java
+++ src/main/java/org/example/controller/LoginController.java
@@ -1,5 +1,7 @@
 package org.example.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class LoginController {
 
-    @GetMapping({"/login","/"})
-    public String login(){
-        return "login";
-    }
+  @GetMapping()
+  public String root() {
+    return "redirect:/welcome";
+  }
 
-    @GetMapping("/welcome")
-    public String welcome(){
-        return "welcome";
+  @GetMapping({"/login"})
+  public String login(@AuthenticationPrincipal UserDetails userDetails) {
+
+    if (userDetails == null) {
+      return "login";
+    } else {
+      return "redirect:/";
     }
+  }
+
+  @GetMapping("/welcome")
+  public String welcome() {
+    return "welcome";
+  }
 }
