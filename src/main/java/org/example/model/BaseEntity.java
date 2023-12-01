cfa859130e5820aa5084c40bce579c0ec2317a5e
JBraddockm <jbraddockm@gmail.com>
2023-12-01 11:54:37 +0000

    Implemented AuditorAware.

--- src/main/java/org/example/model/BaseEntity.java
+++ src/main/java/org/example/model/BaseEntity.java
@@ -1,16 +1,24 @@
 package org.example.model;
 
 import jakarta.persistence.*;
+import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.Instant;
+import org.example.model.common.UserPrincipal;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @MappedSuperclass
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
   //    @SequenceGenerator(
   //            name = "entity_id_seq",
@@ -34,29 +42,49 @@ public abstract class BaseEntity {
       name = "created_on",
       updatable = false,
       nullable = false,
-      columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+      columnDefinition = "TIMESTAMP WITHOUT TIME ZONE",
+      length = 50)
+  @CreatedDate
   private Instant createdOn;
 
-  @Column(name = "created_by", updatable = false, nullable = false)
+  @Column(name = "created_by", updatable = false, nullable = false, length = 50)
+  @CreatedBy
   private Long createdBy;
 
   @Column(name = "updated_by")
+  @LastModifiedBy
   private Long updatedBy;
 
-  @Column(name = "updated_on", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+  @Column(name = "updated_on", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE", length = 50)
+  @LastModifiedDate
   private Instant updatedOn;
 
-  private boolean isDeleted = false;
+  @Column(name ="deleted_on", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE", length = 50)
+  private Instant deletedOn;
+
+  @Column(name = "deleted_by", length = 50)
+  private Long deletedBy;
+
+  @Column(name = "is_deleted", length = 50)
+  private Boolean isDeleted = false;
 
   @PrePersist
-  public void prePersist() {
-    this.createdBy = 1L;
-    this.createdOn = Instant.now();
-  }
-
   @PreUpdate
-  private void preUpdate() {
-    this.updatedOn = Instant.now();
-    this.updatedBy = 1L;
+  public void beforeAnyUpdate() {
+
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    UserPrincipal user = (UserPrincipal) authentication.getPrincipal();
+
+    if (isDeleted != null && isDeleted) {
+
+      if (deletedBy == null) {
+        deletedBy = user.getId();
+      }
+
+      if (getDeletedOn() == null) {
+        deletedOn = Instant.now();
+      }
+    }
   }
 }
