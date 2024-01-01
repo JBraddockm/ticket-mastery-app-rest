91ed5cfda857e72cc500e6cbbe4fada3c670092d
JBraddockm <jbraddockm@gmail.com>
2024-01-01 15:42:29 +0000

    Added openapi annotations. Added JsonIgnoreProperties.

--- src/main/java/org/example/dto/RoleDTO.java
+++ src/main/java/org/example/dto/RoleDTO.java
@@ -1,5 +1,8 @@
 package org.example.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +10,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Schema(name = "Role")
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RoleDTO {
-    private Long id;
-    private String description;
+
+  @Schema(example = "3")
+  private Long id;
+
+  @Schema(example = "Employee")
+  private String description;
 }
