3b545cd00d328c5235e1009c3aeeb31bb2c56b6c
JBraddockm <jbraddockm@gmail.com>
2024-01-01 15:42:56 +0000

    Created GlobalExceptionHandler

---
+++ src/main/java/org/example/dto/ErrorResponse.java
@@ -0,0 +1,34 @@
+package org.example.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+  public static ResponseEntity<Object> buildErrorResponse(
+      HttpStatus httpStatus, String message, List<String> errors, HttpServletRequest request) {
+
+    Map<String, Object> map = new HashMap<>();
+
+    map.put("status", httpStatus.value());
+
+    map.put("timestamp", Instant.now());
+
+    map.put("message", message);
+
+    if (errors.size() == 1) {
+      map.put("error", errors.get(0));
+    } else {
+      map.put("error", errors);
+    }
+
+    map.put("path", request.getRequestURI());
+
+    return new ResponseEntity<>(map, httpStatus);
+  }
+}
---
+++ src/main/java/org/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,63 @@
+package org.example.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.example.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  // TODO Catch BadCredentialsException.class and AccessDeniedException.class after implementing
+  // Spring Security.
+
+  // TODO Enable stacktrace for development environment.
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException e, HttpServletRequest request) {
+
+    List<String> errors = e.getFieldErrors().stream().map(FieldError::getDefaultMessage).toList();
+
+    return ErrorResponse.buildErrorResponse(
+        HttpStatus.UNPROCESSABLE_ENTITY, "Validation Failed", errors, request);
+  }
+
+  @ExceptionHandler({
+    UserNotFoundException.class,
+    ProjectNotFoundException.class,
+    RoleNotFoundException.class,
+    TaskNotFoundException.class
+  })
+  public ResponseEntity<Object> handleEntityObjectNotFound(
+      UserNotFoundException e, HttpServletRequest request) {
+
+    return ErrorResponse.buildErrorResponse(
+        HttpStatus.NOT_FOUND, "Resource Not Found", List.of(e.getMessage()), request);
+  }
+
+  @ExceptionHandler(NoSuchElementException.class)
+  public ResponseEntity<Object> handleItemNotFoundException(
+      NoSuchElementException e, HttpServletRequest request) {
+
+    return ErrorResponse.buildErrorResponse(
+        HttpStatus.NOT_FOUND, "Resource Not Found", List.of(e.getMessage()), request);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<Object> handleAllUncaughtException(
+      Exception e, HttpServletRequest request) {
+
+    return ErrorResponse.buildErrorResponse(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        "Internal Server Error",
+        List.of(e.getMessage()),
+        request);
+  }
+}
