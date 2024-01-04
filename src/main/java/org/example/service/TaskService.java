011e5cdd0bc18167d11b222a328596c677576716
JBraddockm <jbraddockm@gmail.com>
2024-01-04 20:24:54 +0000

    Refactored taskservice

--- src/main/java/org/example/service/TaskService.java
+++ src/main/java/org/example/service/TaskService.java
@@ -1,29 +1,29 @@
 package org.example.service;
 
 import java.util.*;
+
+import org.example.dto.TaskProcessDTO;
 import org.example.dto.TaskDTO;
 import org.example.enums.Status;
-import org.example.model.Task;
 
 public interface TaskService {
 
-  TaskDTO save(TaskDTO taskDTO);
-
-  void update(TaskDTO taskDTO);
-
   Optional<TaskDTO> findById(Long id);
 
-  void deleteById(Long id);
-
   List<TaskDTO> findAll();
+
+  TaskDTO save(TaskDTO taskDTO);
+
+  TaskDTO updateTask(Long id, TaskProcessDTO taskProcessDTO);
+  TaskDTO createTask(TaskProcessDTO taskProcessDTO);
+
+  TaskDTO processTask(TaskProcessDTO taskProcessDTO);
+
+  void deleteById(Long id);
 
   List<TaskDTO> findAllTasksByStatus(Status status);
 
   List<TaskDTO> findAllTasksByStatusIsNot(Status status);
 
-  void updateStatus(TaskDTO task);
-
-  TaskDTO mapToDTO(Task task);
-
-  Task mapToEntity(TaskDTO taskDTO);
+  TaskDTO updateTaskStatus(Long taskId, Status status);
 }
--- src/main/java/org/example/service/impl/TaskServiceImpl.java
+++ src/main/java/org/example/service/impl/TaskServiceImpl.java
@@ -2,84 +2,140 @@ package org.example.service.impl;
 
 import java.util.List;
 import java.util.Optional;
+import org.example.dto.ProjectDTO;
 import org.example.dto.TaskDTO;
+import org.example.dto.TaskProcessDTO;
+import org.example.dto.UserDTO;
 import org.example.enums.Status;
+import org.example.exception.ProjectNotFoundException;
 import org.example.exception.TaskNotFoundException;
+import org.example.exception.UserNotFoundException;
+import org.example.mapper.TaskMapper;
 import org.example.model.Task;
 import org.example.repository.TaskRepository;
+import org.example.service.ProjectService;
 import org.example.service.TaskService;
-import org.modelmapper.ModelMapper;
+import org.example.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TaskServiceImpl extends AbstractCommonService<Task, TaskDTO> implements TaskService {
+public class TaskServiceImpl implements TaskService {
 
   private final TaskRepository taskRepository;
+  private final ProjectService projectService;
+  private final UserService userService;
+  private final TaskMapper taskMapper;
 
   public TaskServiceImpl(
-          ModelMapper modelMapper, TaskRepository taskRepository) {
-    super(modelMapper, Task.class, TaskDTO.class);
+      TaskRepository taskRepository,
+      ProjectService projectService,
+      UserService userService,
+      TaskMapper taskMapper) {
     this.taskRepository = taskRepository;
+    this.projectService = projectService;
+    this.userService = userService;
+    this.taskMapper = taskMapper;
   }
 
   @Override
   public TaskDTO save(TaskDTO taskDTO) {
-    Task task = this.mapToEntity(taskDTO);
+    Task task = taskMapper.mapToEntity(taskDTO);
     taskRepository.save(task);
-    return this.mapToDTO(task);
+    return taskMapper.mapToDTO(task);
+  }
+
+  @Override
+  public TaskDTO processTask(TaskProcessDTO taskProcessDTO) {
+
+    ProjectDTO projectDTO =
+        projectService
+            .findByProjectCode(taskProcessDTO.getProjectCode())
+            .orElseThrow(() -> new ProjectNotFoundException(taskProcessDTO.getProjectCode()));
+
+    UserDTO userDTO =
+        userService
+            .findByUsername(taskProcessDTO.getAssignedEmployee())
+            .orElseThrow(() -> new UserNotFoundException(taskProcessDTO.getAssignedEmployee()));
+
+    TaskDTO taskDTO = new TaskDTO();
+
+    taskDTO.setProject(projectDTO);
+    taskDTO.setAssignedEmployee(userDTO);
+    taskDTO.setTaskDetail(taskProcessDTO.getTaskDetail());
+    taskDTO.setTaskSubject(taskProcessDTO.getTaskSubject());
+    taskDTO.setAssignedDate(taskProcessDTO.getAssignedDate());
+
+    if (taskProcessDTO.getId() != null) {
+      this.findById(taskProcessDTO.getId()).ifPresent(task -> taskDTO.setStatus(task.getStatus()));
+    } else {
+      taskDTO.setStatus(Status.OPEN);
+    }
+
+    taskDTO.setId(taskProcessDTO.getId());
+
+    return this.save(taskDTO);
   }
 
   @Override
   public List<TaskDTO> findAll() {
-    return taskRepository.findAll().stream().map(this::mapToDTO).toList();
+    return taskRepository.findAll().stream().map(taskMapper::mapToDTO).toList();
   }
 
   @Override
-  public void update(TaskDTO taskDTO) {
+  public TaskDTO updateTask(Long taskId, TaskProcessDTO taskProcessDTO) {
 
-    Task task =
-        taskRepository
-            .findById(taskDTO.getId())
-            .orElseThrow(() -> new TaskNotFoundException(taskDTO.getId().toString()));
+    this.findById(taskId)
+        .ifPresentOrElse(
+            task -> taskProcessDTO.setId(taskId),
+            () -> {
+              throw new TaskNotFoundException(String.valueOf(taskId));
+            });
 
-    Task updatedTask = this.mapToEntity(taskDTO);
-    updatedTask.setId(task.getId());
-    updatedTask.setStatus(task.getStatus());
-
-    taskRepository.save(updatedTask);
+    return this.processTask(taskProcessDTO);
   }
 
   @Override
-  public void updateStatus(TaskDTO taskDTO) {
-
-    Task task =
-        taskRepository
-            .findById(taskDTO.getId())
-            .orElseThrow(() -> new TaskNotFoundException(taskDTO.getId().toString()));
-
-    task.setStatus(taskDTO.getStatus());
-
-    taskRepository.save(task);
+  public TaskDTO createTask(TaskProcessDTO taskProcessDTO) {
+    return this.processTask(taskProcessDTO);
   }
 
   @Override
-  public void deleteById(Long id) {
-    taskRepository.deleteById(id);
+  public TaskDTO updateTaskStatus(Long taskId, Status status) {
+
+    TaskDTO taskDTO = this.findById(taskId).orElseThrow(() -> new TaskNotFoundException(String.valueOf(taskId)));
+
+    taskDTO.setStatus(status);
+
+    return this.save(taskDTO);
+  }
+
+  @Override
+  public void deleteById(Long taskId) {
+    // TODO Implement soft delete
+    this.findById(taskId)
+        .ifPresentOrElse(
+            task -> taskRepository.deleteById(taskId),
+            () -> {
+              throw new TaskNotFoundException(String.valueOf(taskId));
+            });
   }
 
   @Override
   public Optional<TaskDTO> findById(Long id) {
-    return taskRepository.findById(id).map(this::mapToDTO);
+    return taskRepository.findById(id).map(taskMapper::mapToDTO);
   }
 
   @Override
   public List<TaskDTO> findAllTasksByStatus(Status status) {
-    return taskRepository.findTasksByStatusIs(status).stream().map(this::mapToDTO).toList();
+    // TODO Only retrieve tasks by an logged in Employee.
+    return taskRepository.findTasksByStatusIs(status).stream().map(taskMapper::mapToDTO).toList();
   }
 
   @Override
   public List<TaskDTO> findAllTasksByStatusIsNot(Status status) {
-    return taskRepository.findTasksByStatusIsNot(status).stream().map(this::mapToDTO).toList();
+    // TODO Only retrieve tasks by an logged in Employee.
+    return taskRepository.findTasksByStatusIsNot(status).stream()
+        .map(taskMapper::mapToDTO)
+        .toList();
   }
-
 }
