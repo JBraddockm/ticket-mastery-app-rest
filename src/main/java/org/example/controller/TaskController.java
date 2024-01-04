c3d8a6428caeece7c2f89d5dd7133e0f73f20c8e
JBraddockm <jbraddockm@gmail.com>
2024-01-04 20:25:41 +0000

    Refactored and reimplemented TaskController for REST with Openapi 3 documentation.

--- src/main/java/org/example/controller/TaskController.java
+++ src/main/java/org/example/controller/TaskController.java
@@ -1,229 +1,234 @@
 package org.example.controller;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Set;
-import org.example.dto.ProjectDTO;
+import java.util.Map;
 import org.example.dto.TaskDTO;
-import org.example.dto.UserDTO;
+import org.example.dto.TaskProcessDTO;
 import org.example.enums.Status;
 import org.example.exception.TaskNotFoundException;
-import org.example.exception.UserNotFoundException;
-import org.example.model.Task;
-import org.example.service.ProjectService;
 import org.example.service.TaskService;
-import org.example.service.UserService;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
-@RequestMapping("/task")
+@RestController
+@RequestMapping("/api/v1/tasks")
+@Tag(
+    name = "task",
+    description = "Everything about Task",
+    externalDocs =
+        @ExternalDocumentation(
+            url = "https://github.com/jbraddockm",
+            description = "Find out more"))
 public class TaskController {
-
   private final TaskService taskService;
-  private final UserService userService;
-  private final ProjectService projectService;
 
-  public TaskController(
-      TaskService taskService, UserService userService, ProjectService projectService) {
+  public TaskController(TaskService taskService) {
     this.taskService = taskService;
-    this.userService = userService;
-    this.projectService = projectService;
   }
 
-  @ModelAttribute("projects")
-  public List<ProjectDTO> getAllProjects(@AuthenticationPrincipal UserDetails userDetails) {
-
-    UserDTO manager =
-        userService
-            .findByUsername(userDetails.getUsername())
-            .orElseThrow(() -> new UserNotFoundException(userDetails.getUsername()));
-    return projectService.findAllByProjectManagerIs(manager);
+  @GetMapping
+  @Operation(
+      summary = "Get a list of tasks",
+      description = "Returns a list of tasks",
+      operationId = "readAllTasks")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema =
+                        @Schema(name = "Task", implementation = TaskDTO.class, title = "Task")))
+      })
+  public List<TaskDTO> readAllTasks() {
+    return taskService.findAll();
   }
 
-  @ModelAttribute("employees")
-  public List<UserDTO> getAllEmployees() {
-    return userService.findAllEmployees();
+  @GetMapping("{taskId}")
+  @Operation(
+      summary = "Find task by ID",
+      description = "Returns a single task",
+      operationId = "getTaskById")
+  @Parameters(@Parameter(name = "taskId", description = "ID of task to return", example = "1"))
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema =
+                        @Schema(name = "Task", implementation = TaskDTO.class, title = "Task"))),
+        @ApiResponse(responseCode = "404", description = "Task not found", content = @Content())
+      })
+  public TaskDTO getTaskById(@PathVariable("taskId") Long taskId) {
+    return taskService
+        .findById(taskId)
+        .orElseThrow(() -> new TaskNotFoundException(String.valueOf(taskId)));
   }
 
-  @GetMapping("all")
-  public String viewAllTasks(Model model, @AuthenticationPrincipal UserDetails userDetails) {
-
-    UserDTO manager =
-        userService
-            .findByUsername(userDetails.getUsername())
-            .orElseThrow(() -> new UserNotFoundException(userDetails.getUsername()));
-
-    // TODO Extract this to the project or task service.
-    List<Task> tasksList =
-        projectService.getCountedListOfProjectDTO(manager).stream()
-            .map(ProjectDTO::getTasks)
-            .flatMap(Set::stream)
-            .toList();
-
-    model.addAttribute("tasks", tasksList);
-    return "task/list";
+  @PostMapping
+  @Operation(
+      summary = "Create a task",
+      description = "Create a single task",
+      operationId = "createTask")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema =
+                        @Schema(
+                            name = "Task",
+                            implementation = TaskProcessDTO.class,
+                            title = "Task"))),
+        @ApiResponse(responseCode = "422", description = "Validation Error", content = @Content())
+      })
+  public TaskDTO createTask(@Valid @RequestBody TaskProcessDTO taskProcessDTO) {
+    return taskService.createTask(taskProcessDTO);
   }
 
-  @GetMapping("/create")
-  public String newCreateTask(Model model, TaskDTO task) {
-
-    model.addAttribute("task", task);
-
-    return "task/create";
+  @PutMapping("{taskId}")
+  @Operation(
+      summary = "Update an existing task",
+      description = "Update an existing task",
+      operationId = "updateTask")
+  @Parameters(@Parameter(name = "taskId", description = "ID of task to update", example = "1"))
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema =
+                        @Schema(
+                            name = "Task",
+                            implementation = TaskProcessDTO.class,
+                            title = "Task"))),
+        @ApiResponse(responseCode = "404", description = "Task not found", content = @Content()),
+        @ApiResponse(responseCode = "422", description = "Validation Error", content = @Content())
+      })
+  public TaskDTO updateTask(
+      @Valid @RequestBody TaskProcessDTO taskProcessDTO, @PathVariable("taskId") Long taskId) {
+    return taskService.updateTask(taskId, taskProcessDTO);
   }
 
-  @PostMapping("/create")
-  public String newCreateTask(
-      @Valid @ModelAttribute("task") TaskDTO task,
-      BindingResult result,
-      RedirectAttributes redirectAttributes,
-      Model model) {
-
-    if (result.hasErrors()) {
-      model.addAttribute(
-          "isValid", "bg-green-50 border-green-500 text-green-900 dark:border-green-400 ");
-      return "task/create";
-    } else {
-      task.setAssignedDate(LocalDate.now());
-      task.setStatus(Status.OPEN);
-
-      taskService.save(task);
-
-      // TODO Retrieve the task object from the database and get its ID. Currently, it is empty.
-      redirectAttributes.addFlashAttribute("createdTask", task.getId());
-    }
-
-    return "redirect:/task/all";
-  }
-
-  // New
-  @PostMapping("{taskID}/delete")
-  public String newDeleteTask(
-      @PathVariable("taskID") Long taskID, RedirectAttributes redirectAttributes) {
-
-    TaskDTO taskDTO =
-        taskService
-            .findById(taskID)
-            .orElseThrow(() -> new TaskNotFoundException(taskID.toString()));
-
-    taskService.deleteById(taskID);
-
-    redirectAttributes.addFlashAttribute("deletedTask", taskDTO.getId());
-
-    return "redirect:/task/all";
-  }
-
-  @GetMapping("{taskID}/edit")
-  public String newEditTask(@PathVariable("taskID") Long taskID, Model model) {
-
-    TaskDTO updatedTask =
-        taskService
-            .findById(taskID)
-            .orElseThrow(() -> new TaskNotFoundException(taskID.toString()));
-
-    model.addAttribute("task", updatedTask);
-
-    return "task/update";
-  }
-
-  @PostMapping("{taskID}/edit")
-  public String newEditTask(
-      @Valid @ModelAttribute("task") TaskDTO task,
-      BindingResult result,
-      @PathVariable("taskID") Long taskID,
-      RedirectAttributes redirectAttributes,
-      Model model) {
-
-    if (result.hasErrors()) {
-      model.addAttribute(
-          "isValid", "bg-green-50 border-green-500 text-green-900 dark:border-green-400 ");
-      return "task/update";
-    }
-
-    TaskDTO updatedTask =
-        taskService
-            .findById(taskID)
-            .orElseThrow(() -> new TaskNotFoundException(taskID.toString()));
-
-    if (task.getId().equals(taskID)) {
-      taskService.update(task);
-      redirectAttributes.addFlashAttribute("updatedTask", updatedTask.getId());
-    }
-
-    return "redirect:/task/all";
+  @DeleteMapping("{taskId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @Operation(
+      summary = "Delete a task",
+      description = "Deletes a single task",
+      operationId = "deleteTask")
+  @Parameters(@Parameter(name = "taskId", description = "ID of task to delete", example = "1"))
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "204",
+            description = "Successful operation",
+            content = @Content()),
+        @ApiResponse(responseCode = "404", description = "Invalid task value", content = @Content())
+      })
+  public void deleteTask(@PathVariable("taskId") Long taskId) {
+    taskService.deleteById(taskId);
   }
 
   @GetMapping("/employee/pending-tasks")
-  public String newEmployeePendingTasks(Model model) {
-    // TODO This should only get tasks of a particular logged in employee after Spring Security.
-    model.addAttribute("pendingTasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETED));
-    return "task/pending-tasks";
+  @Operation(
+      summary = "Get a list of pending tasks",
+      description = "Returns a list of an employee's pending tasks",
+      operationId = "employeePendingTasks")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema =
+                        @Schema(name = "Task", implementation = TaskDTO.class, title = "Task")))
+      })
+  public List<TaskDTO> employeePendingTasks() {
+    return taskService.findAllTasksByStatusIsNot(Status.COMPLETED);
   }
 
-  @GetMapping("/employee/{id}/edit")
-  public String employeeEditTask(@PathVariable("id") Long id, Model model) {
-
-    TaskDTO task =
-        taskService.findById(id).orElseThrow(() -> new TaskNotFoundException(id.toString()));
-
-    model.addAttribute("task", task);
-    model.addAttribute("statuses", Status.values());
-
-    return "task/status-update";
-  }
-
-  @PostMapping("/employee/{id}/edit")
-  public String employeeUpdateTask(
-      @Valid @ModelAttribute("task") TaskDTO task,
-      BindingResult result,
-      @PathVariable("id") Long id,
-      RedirectAttributes redirectAttributes,
-      Model model) {
-
-    if (result.hasErrors()) {
-      model.addAttribute(
-          "isValid", "bg-green-50 border-green-500 text-green-900 dark:border-green-400 ");
-      model.addAttribute("statuses", Status.values());
-      return "task/status-update";
-    }
-
-    TaskDTO updatedTask =
-        taskService.findById(id).orElseThrow(() -> new TaskNotFoundException(id.toString()));
-
-    if (task.getId().equals(id)) {
-      taskService.updateStatus(task);
-      redirectAttributes.addFlashAttribute("updatedTask", updatedTask.getId());
-    }
-
-    return "redirect:/task/employee/pending-tasks";
+  @PutMapping("/employee/{taskId}/status")
+  @Operation(
+      summary = "Update status of an existing task",
+      description = "Updates status of an existing task",
+      operationId = "updateTaskStatus")
+  @Parameters(
+      @Parameter(name = "taskId", description = "ID of task to update the status", example = "1"))
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema =
+                        @Schema(name = "Task", type = "Task", implementation = TaskDTO.class))),
+        @ApiResponse(
+            responseCode = "404",
+            description = "Invalid status value provided",
+            content = @Content())
+      })
+  public TaskDTO updateTaskStatus(
+      @PathVariable("taskId") Long taskId,
+      @io.swagger.v3.oas.annotations.parameters.RequestBody(
+              description = "Map with key-value pairs representing status details",
+              content =
+                  @Content(
+                      examples = @ExampleObject(value = "status: Open"),
+                      schema =
+                          @Schema(
+                              title = "Status",
+                              name = "Status",
+                              type = "string",
+                              allowableValues = "Open, Completed, In Progress")))
+          @RequestBody
+          Map<String, String> status) {
+    return taskService.updateTaskStatus(taskId, Status.fromText(status.get("status")));
   }
 
   @GetMapping("/employee/archive")
-  public String employeeArchivedTasks(Model model) {
-    model.addAttribute("completedTasks", taskService.findAllTasksByStatus(Status.COMPLETED));
-    return "task/archive";
+  @Operation(
+      summary = "Get the list of completed tasks",
+      description = "Returns the list of completed tasks of an employee",
+      operationId = "employeeArchivedTasks")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Successful operation",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema =
+                        @Schema(name = "Task", implementation = TaskDTO.class, title = "Task")))
+      })
+  public List<TaskDTO> employeeArchivedTasks() {
+    return taskService.findAllTasksByStatus(Status.COMPLETED);
   }
-
-  // task/employee/id/status
-  //  @PostMapping("/employee/{id}/status")
-  //  public String updateTaskStatus(
-  //      @PathVariable("id") Long id, @RequestParam String status, Model model) {
-  //    // Change the status.
-  //
-  //    taskService.findById(id).setStatus(Status.valueOf(status));
-  //
-  //    // Add the user selected status to the partial.
-  //    model.addAttribute("status_selected", status);
-  //
-  //    // Add all statuses to the partial.
-  //    model.addAttribute("statuses", Status.values());
-  //
-  //    return "fragments/partials/status-response :: status";
 }
