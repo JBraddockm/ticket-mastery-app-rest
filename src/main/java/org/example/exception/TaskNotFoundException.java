4290a6c6f71836b9e72a1d1aa0f48b6494eec715
JBraddockm <jbraddockm@gmail.com>
2024-01-04 18:21:31 +0000

    Changed exception message.

--- src/main/java/org/example/exception/TaskNotFoundException.java
+++ src/main/java/org/example/exception/TaskNotFoundException.java
@@ -3,6 +3,6 @@ package org.example.exception;
 public class TaskNotFoundException extends RuntimeException {
 
     public TaskNotFoundException(String message) {
-        super(message);
+        super(String.format("Task with the id %s could not be found", message));
     }
 }
