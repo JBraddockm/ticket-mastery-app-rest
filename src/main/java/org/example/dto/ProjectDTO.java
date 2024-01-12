1699b4168721235881f58c8913c6541d1ceb86c4
JBraddockm <jbraddockm@gmail.com>
2024-01-12 11:31:53 +0000

    Custom getters for completedTaskCount pendingTaskCount

--- src/main/java/org/example/dto/ProjectDTO.java
+++ src/main/java/org/example/dto/ProjectDTO.java
@@ -1,63 +1,84 @@
 package org.example.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.annotation.ValidDueDate;
 import org.example.enums.Status;
-import org.example.model.Task;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @ValidDueDate(
-        startDate = "projectStartDate",
-        endDate = "projectEndDate",
-        message = "Choose a valid due date"
-)
+    startDate = "projectStartDate",
+    endDate = "projectEndDate",
+    message = "Choose a valid due date")
+@Schema(name = "Project")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProjectDTO {
 
-    Long id;
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+  @Schema(example = "1")
+  private Long id;
 
-    @NotBlank(
-            message = "Add project name"
-    )
-    private String projectName;
+  @NotBlank(message = "Add project name")
+  @Schema(example = "Interface Projection in Spring Data JPA")
+  private String projectName;
 
-    @NotBlank(
-            message = "Add a project code"
-    )
-    private String projectCode;
+  @NotBlank(message = "Add a project code")
+  @Schema(example = "PR001")
+  private String projectCode;
 
-    @NotNull(
-            message = "Choose a manager"
-    )
-    private UserDTO projectManager;
+  @NotNull(message = "Choose a manager")
+  private UserDTO projectManager;
 
-    private Set<Task> tasks = new HashSet<>();
+  @JsonIgnore
+  // TODO Check out JsonBackReference.
+  private List<TaskDTO> tasks = new ArrayList<>();
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotNull(message = "Choose a start date")
-    private LocalDate projectStartDate;
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  @NotNull(message = "Choose a start date")
+  private LocalDate projectStartDate;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotNull(message = "Choose a due date")
-    private LocalDate projectEndDate;
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  @NotNull(message = "Choose a due date")
+  private LocalDate projectEndDate;
 
-    @NotBlank(
-            message = "Add details about the project"
-    )
-    private String projectDetail;
+  @NotBlank(message = "Add details about the project")
+  @Schema(example = "Your project details")
+  private String projectDetail;
 
-    private Status projectStatus;
+  private Status projectStatus;
 
-    private int completeTaskCounts;
-    private int unfinishedTaskCounts;
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+  private int completedTaskCount;
 
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+  private int pendingTaskCount;
+
+  public int getCompletedTaskCount() {
+    return getProjectStatusByTask().get(true).size();
+  }
+
+  public int getPendingTaskCount() {
+    return getProjectStatusByTask().get(false).size();
+  }
+
+  private Map<Boolean, List<TaskDTO>> getProjectStatusByTask() {
+    return this.getTasks().stream()
+        .collect(
+            Collectors.partitioningBy(taskDTO -> taskDTO.getStatus().equals(Status.COMPLETED)));
+  }
 }
