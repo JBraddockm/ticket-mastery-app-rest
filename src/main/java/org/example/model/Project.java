487f3d5c5fa5a7f1ab69b0894b9ed10a74b06313
JBraddockm <jbraddockm@gmail.com>
2024-01-12 11:31:02 +0000

    Formatted with google-java-format.

--- src/main/java/org/example/model/Project.java
+++ src/main/java/org/example/model/Project.java
@@ -1,61 +1,43 @@
 package org.example.model;
 
 import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.example.enums.Status;
 
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
-
 @Entity
 @Table(
-        name = "projects",
-        uniqueConstraints = {
-                @UniqueConstraint(
-                        name = "project_code_unique",
-                        columnNames = "project_code"
-                )
-        }
-)
+    name = "projects",
+    uniqueConstraints = {
+      @UniqueConstraint(name = "project_code_unique", columnNames = "project_code")
+    })
 @NoArgsConstructor
 @Getter
 @Setter
-public class Project extends BaseEntity{
+public class Project extends BaseEntity {
 
-    private String projectName;
+  private String projectName;
 
-    @Column(
-            name = "project_code"
-    )
-    private String projectCode;
+  @Column(name = "project_code")
+  private String projectCode;
 
-    @ManyToOne(
-            fetch = FetchType.LAZY
-    )
-    @JoinColumn(
-            name = "manager_id",
-            foreignKey = @ForeignKey(
-                    name = "project_manager_id_fk"
-            )
-    )
-    private User projectManager;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "manager_id", foreignKey = @ForeignKey(name = "project_manager_id_fk"))
+  private User projectManager;
 
-    @OneToMany(
-            orphanRemoval = true,
-            mappedBy = "project"
-    )
-    private Set<Task> tasks = new HashSet<>();
+  @OneToMany(orphanRemoval = true, mappedBy = "project")
+  private List<Task> tasks = new ArrayList<>();
 
-    private LocalDate projectStartDate;
+  private LocalDate projectStartDate;
 
-    private LocalDate projectEndDate;
+  private LocalDate projectEndDate;
 
-    private String projectDetail;
+  private String projectDetail;
 
-    @Enumerated(EnumType.STRING)
-    private Status projectStatus;
-
+  @Enumerated(EnumType.STRING)
+  private Status projectStatus;
 }
