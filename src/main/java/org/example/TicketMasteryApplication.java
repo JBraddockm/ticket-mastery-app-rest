6038a3d2a2a893bb5550ff7c23e3ab3bb4581327
JBraddockm <jbraddockm@gmail.com>
2024-01-18 12:40:02 +0000

    Formatted with google-java-format.

--- src/main/java/org/example/TicketMasteryApplication.java
+++ src/main/java/org/example/TicketMasteryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicketMasteryApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(TicketMasteryApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(TicketMasteryApplication.class, args);
+  }
 }
