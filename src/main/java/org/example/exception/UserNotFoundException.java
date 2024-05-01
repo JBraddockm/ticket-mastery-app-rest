e206e10cefab94b7ff0d42dbcebabb7a0fc64311
JBraddockm <jbraddockm@gmail.com>
2024-05-01 20:25:16 +0100

    Made exception message more generic

--- src/main/java/org/example/exception/UserNotFoundException.java
+++ src/main/java/org/example/exception/UserNotFoundException.java
@@ -2,6 +2,6 @@ package org.example.exception;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String username) {
-        super(String.format("User with email %s could not be found", username));
+        super(String.format("User %s could not be found", username));
     }
 }
